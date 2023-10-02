241b9790fc21e4c4becc4f2e3e8bf08c3b8faec5
Ivan Levchuk <swankyyy1@gmail.com>
2023-10-02 12:59:47 +0300

    4.9

--- store/users/views.py
+++ store/users/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from users.models import User
+from users.forms import UserLoginForm
+from django.contrib import auth
+from django.urls import reverse
+
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('products:index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context=context)
+
 
 def registration(request):
     return render(request, 'users/registration.html')
+
+
