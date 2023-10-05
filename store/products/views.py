4510c3b0bdafc55672a85af069d66fa8ffa57584
swank <swankyyy1@gmail.com>
2023-10-05 11:21:29 +0300

    add some products and change view products list

--- store/products/views.py
+++ store/products/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .models import Product
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'products/index.html')
 
+
 def products(request):
-    return render(request, 'products/products.html')
+    content = Product.objects.all()
+    context = {'items': content}
+    return render(request, 'products/products.html', context=context)
