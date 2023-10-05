ae585f12cebff358d8d858021f682e84f8ee2f46
swank <swankyyy1@gmail.com>
2023-10-05 20:17:36 +0300

    5.4 done

--- store/products/models.py
+++ store/products/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from users.models import User
 
+
 class ProductCategory(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField(blank=True)
-    
+
     def __str__(self) -> str:
         return f'{self.name}'
-    
-    
+
+
 class Product(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField(blank=True)
@@ -18,11 +19,23 @@ class Product(models.Model):
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
 
 
+class BasketQuerySet(models.QuerySet):
+    def total_sum(self):
+        return sum(basket.sum() for basket in self)
+
+    def total_quantity(self):
+        return sum(basket.quantity for basket in self)
+
+
 class Basket(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
     quantity = models.SmallIntegerField(default=1)
     created_timestamp = models.DateTimeField(auto_now_add=True)
+    objects = BasketQuerySet.as_manager()
 
     def __str__(self):
         return f'Корзина для {self.user.email} | Продукт {self.product.name}'
+
+    def sum(self):
+        return self.product.price * self.quantity
--- store/products/urls.py
+++ store/products/urls.py
@@ -8,3 +8,4 @@ urlpatterns = [
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
     path('baskets/remove/<int:id>/', basket_remove, name='basket_remove')
 ]
+
